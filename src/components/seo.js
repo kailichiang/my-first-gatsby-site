1f0cf10258505276835a198505dd74b26abd0857
kailichiang <kelichiang@gmail.com>
2023-05-10 10:48:55 +0800

    Use useStaticQuery to create an Seo component

---
+++ src/components/seo.js
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { graphql, useStaticQuery } from "gatsby";
+
+const Seo = ({ title }) => {
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      site(siteMetadata: {}) {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  return (
+    <title>
+      {title} | {data.site.siteMetadata.title}
+    </title>
+  );
+};
+
+export default Seo;
